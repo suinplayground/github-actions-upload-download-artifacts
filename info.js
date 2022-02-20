7ab6274872ec3443698e5360651c4b58767006ac
suin <suinyeze@gmail.com>
2022-02-20 09:40:50 +0900

    ci: 🎡 test query artifacts

--- info.js
+++ info.js
@@ -1,1 +1,16 @@
-console.log(process.env);
+const github = require('@actions/github')
+
+console.log({context: github.context})
+
+console.log({TOKEN_SIZE: process.env.GITHUB_TOKEN?.length});
+
+(async () => {
+    const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
+    const result = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/artifacts', {
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        run_id: github.context.runId,
+    })
+
+    console.log(result)
+})()
