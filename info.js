985a641b214e95bb0c70fc61c910ca0caab42035
suin <suinyeze@gmail.com>
2022-02-20 09:55:16 +0900

    ci: 🎡 separate jobs

--- info.js
+++ info.js
@@ -6,11 +6,19 @@ console.log({TOKEN_SIZE: process.env.GITHUB_TOKEN?.length});
 
 (async () => {
     const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
-    const result = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/artifacts', {
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        run_id: github.context.runId,
-    })
 
-    console.log(result)
+    while (true) {
+        const result = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/artifacts', {
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            run_id: github.context.runId,
+        })
+
+        console.log(result.data.artifacts)
+
+        if (result.data.total_count > 0) {
+            break
+        }
+        await new Promise(resolve => setTimeout(resolve, 5000))
+    }
 })()
